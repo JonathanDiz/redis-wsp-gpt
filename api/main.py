63f7c27fd2d19e0486edff995d511bc63f925749
JonathanDiz <jdiazolivares007@gmail.com>
2023-04-18 02:01:56 -0400

    configuracion enviar y recibir mensajes con el chatbot y guardarlos en redis

--- api/main.py
+++ api/main.py
@@ -1,14 +1,10 @@
-#ChatBot inteligente con WhatsApp en Python
 from flask import Flask, jsonify, request
 import redis
-import datetime
 
 app = Flask(__name__)
 
-#RUTA DE HOME
-@app.route('/', methods=['GET', 'POST'])
-def home():
-   return "HELLO from vercel use flask"
+# CONECTAMOS A REDIS
+redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 #CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
 @app.route("/webhook/", methods=["POST", "GET"])
@@ -16,7 +12,7 @@ def webhook_whatsapp():
     #SI HAY DATOS RECIBIDOS VIA GET
     if request.method == "GET":
         #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
-        if request.args.get('hub.verify_token') == "HolaChatbotGPT":
+        if request.args.get('hub.verify_token') == "HolaNovato":
             #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
             return request.args.get('hub.challenge')
         else:
@@ -27,54 +23,46 @@ def webhook_whatsapp():
     #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
     telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
     #EXTRAEMOS EL TELEFONO DEL CLIENTE
-    mensaje_data=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
     #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
     idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
     #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
     timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
     #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
     #SI HAY UN MENSAJE
-    if mensaje_data is not None:
+    if mensaje is not None:
       from rivescript import RiveScript
       #INICIALIZAMOS RIVESCRIPT Y CARGAMOS LA CONVERSACION
       bot = RiveScript()
       bot.load_file('restaurante.rive')
       bot.sort_replies()
       #OBTENEMOS LA RESPUESTA
-      respuesta= bot.reply("localuser", mensaje_data)
-      respuesta= respuesta.replace("\\n", "\\\n")
-      respuesta= respuesta.replace("\\","")
-
-      #CONECTAMOS A BASE DE DATOS REDIS
-      redis_host = redis_host.env.REDIS_HOST
-      redis_port = redis_port.env.REDIS_PORT
-      redis_password = redis_password.env.PASSWORD
-
-      redis_conn = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-
-      #CONFIGURACION REGISTRO REDIS
-      id = 1
-      fecha_hora = datetime.datetime.now().date() + " " + datetime.datetime.now().time()
-      mensaje_recibido = mensaje_data.replace("\n", "\\n")
-      mensaje_enviado = respuesta.replace("\\n", "\\\n")
-      id_wsp = idWA
-      timestamp_wsp = timestamp
-      telefono_wsp = telefonoCliente
-
-      redis_conn.hset("registro:" + str(id), "id", id)
-      redis_conn.hset("registro:" + str(id), "fecha_hora", fecha_hora)
-      redis_conn.hset("registro:" + str(id), "mensaje_recibido", mensaje_recibido)
-      redis_conn.hset("registro:" + str(id), "mensaje_enviado", mensaje_enviado)
-      redis_conn.hset("registro:" + str(id), "id_wsp", id_wsp)
-      redis_conn.hset("registro:" + str(id), "timestamp_wsp", timestamp_wsp)
-      redis_conn.hset("registro:" + str(id), "telefono_wsp", telefono_wsp)
-
-      f = open("texto.txt", "w")
-      f.write(mensaje_data + "\n")
-      f.close()
+      respuesta= bot.reply("localuser",mensaje)
+      respuesta=respuesta.replace("\\n","\\\n")
+      respuesta=respuesta.replace("\\","")
+      
+      # AÑADIMOS LOS DATOS A REDIS
+      cantidad = redis_db.get(idWA)
+      if cantidad is None:
+          redis_db.set(idWA, 1)
+          
+          # Insertamos los datos en Redis
+          registro = {
+              'mensaje_recibido': mensaje,
+              'mensaje_enviado': respuesta,
+              'id_wa': idWA,
+              'timestamp_wa': timestamp,
+              'telefono_wa': telefonoCliente
+          }
+          redis_db.hmset(idWA, registro)
+      else:
+          # Actualizamos la cantidad
+          cantidad = int(cantidad) + 1
+          redis_db.set(idWA, cantidad)
+          
       #RETORNAMOS EL STATUS EN UN JSON
       return jsonify({"status": "success"}, 200)
 
 #INICIAMSO FLASK
 if __name__ == "__main__":
-  app.run(debug=True)
+   app.run(debug=True)
